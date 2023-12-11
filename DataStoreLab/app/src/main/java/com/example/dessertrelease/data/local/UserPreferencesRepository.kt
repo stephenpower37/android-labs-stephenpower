eaa51b0d9c4bb557ca563af09500505123e35445
Stephen Power <stephenxpower2003@gmail.com>
2023-12-11 18:32:49 +0000

    DataStore Lab Complete

---
+++ DataStoreLab/app/src/main/java/com/example/dessertrelease/data/local/UserPreferencesRepository.kt
@@ -0,0 +1,39 @@
+package com.example.dessertrelease.data.local
+
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class UserPreferencesRepository(private val dataStore: DataStore<Preferences>) {
+    private companion object {
+        val IS_LINEAR_LAYOUT = booleanPreferencesKey("is_linear_layout")
+        const val TAG = "UserPreferencesRepo"
+    }
+
+        suspend fun saveLayoutPreference(isLinearLayout: Boolean) {
+            dataStore.edit { mutablePreferences ->
+                mutablePreferences[IS_LINEAR_LAYOUT] = isLinearLayout
+            }
+        }
+
+    val isLinearLayout: Flow<Boolean> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                Log.e(TAG, "Error reading preferences.", exception)
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            preferences[IS_LINEAR_LAYOUT] ?: true
+        }
+
+}
